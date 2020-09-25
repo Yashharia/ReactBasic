0c1c66344d1d00bdad67ae09b5ee3dd1ce54957b
yash.rh <yash.rh@somaiya.edu>
2020-09-25 11:59:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import Tweet from './Tweet';
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const [users, setUsers] = useState([
+    { name: "Ed", message: "hello world" },
+    { name: "Ed", message: "hello world" },
+    { name: "Ed", message: "hello world" },
+    { name: "Ed", message: "hello world" }
+  ]);
+
+
+  const increamt = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className={isRed ? "red" : ""}>Change my color</h1>
+      <button onClick={increamt}>Increament</button>
+      <h1>{count}</h1>
+
+      {users.map(user => (
+        <Tweet name={user.name} message={user.nessage} />
+      ))}
     </div>
   );
 }
+
+
 
 export default App;
