ec003b5c50c293f6874baad2fc73b6bd57b23ea9
yash.rh <yash.rh@somaiya.edu>
2020-09-25 12:53:38 +0530

    Updates

---
+++ precache-manifest.902d20fee64d6b2e28c227f608a64034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8a36794a927d289e359e6336a8a1fe",
+    "url": "/ReactBasic/index.html"
+  },
+  {
+    "revision": "055f4c2c819237ba9f97",
+    "url": "/ReactBasic/static/css/main.c01e1ad0.chunk.css"
+  },
+  {
+    "revision": "c4e4695c1ea4e5266c1d",
+    "url": "/ReactBasic/static/js/2.0988551c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactBasic/static/js/2.0988551c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055f4c2c819237ba9f97",
+    "url": "/ReactBasic/static/js/main.808bdcc7.chunk.js"
+  },
+  {
+    "revision": "c4c7e608299d0b68ae74",
+    "url": "/ReactBasic/static/js/runtime-main.2e9a0498.js"
+  }
+]);
